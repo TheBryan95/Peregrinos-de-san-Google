4e2d07ae1cccb7ede857a60f726afd7e3b1ab497
Lukas Avello <lavello21@alumnos.utalca.cl>
2023-05-14 22:34:01 -0400

    Mejora de elementos visuales de ventana menú

--- Prueba-para-Proyecto-1/src/prueba/para/proyecto/pkg1/Main.java
+++ Prueba-para-Proyecto-1/src/prueba/para/proyecto/pkg1/Main.java
@@ -1,11 +1,14 @@
 package prueba.para.proyecto.pkg1;
 
+import java.io.File;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -14,10 +17,11 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         Label l1 = new Label("Menu de ordenamientos Visuales");
         
-        l1.setScaleX(1);
-        l1.setScaleY(1);
+        l1.setScaleX(2);
+        l1.setScaleY(2);
         l1.setStyle("-fx-font-weight: bold;");
-        l1.setLayoutX(7);
+        l1.setLayoutX(235);
+        l1.setLayoutY(10);
         
         Label l2 = new Label();
         l2.setLayoutX(15);
@@ -26,20 +30,31 @@ public class Main extends Application {
         Button botonBubble = new Button("Bubble Sort");
         Button botonCocktail = new Button("Cocktail Sort");
         
-        botonInsert.setLayoutX(50);
-        botonInsert.setLayoutY(60);
+        String imagePath = "menu.jpg";
+        Image image = new Image(new File(imagePath).toURI().toString());
+        ImageView imageView = new ImageView(image);
         
-        botonInsert.setPrefWidth(100);
-        botonBubble.setLayoutX(50);
-        botonBubble.setLayoutY(100);
-        botonBubble.setPrefWidth(100);
         
-        botonCocktail.setLayoutX(50);
-        botonCocktail.setLayoutY(140);
-        botonCocktail.setPrefWidth(100);
+        botonInsert.setLayoutX(230);
+        botonInsert.setLayoutY(200);
+        botonInsert.setPrefWidth(200);
+        botonInsert.setScaleX(2);
+        botonInsert.setScaleY(2);
         
-        Group root = new Group(l1,l2,botonInsert,botonBubble,botonCocktail);
-        Scene scene = new Scene(root, 200, 200);
+        botonBubble.setLayoutX(230);
+        botonBubble.setLayoutY(450);
+        botonBubble.setPrefWidth(200);
+        botonBubble.setScaleX(2);
+        botonBubble.setScaleY(2);
+        
+        botonCocktail.setLayoutX(230);
+        botonCocktail.setLayoutY(680);
+        botonCocktail.setPrefWidth(200);
+        botonCocktail.setScaleX(2);
+        botonCocktail.setScaleY(2);
+        
+        Group root = new Group(imageView,l1,l2,botonInsert,botonBubble,botonCocktail);
+        Scene scene = new Scene(root, 640, 960);
         scene.setFill(Color.LIGHTSKYBLUE);
         primaryStage.setScene(scene);
         primaryStage.show();
